b25df888988740b37c5140aa3f3bbafcb2375bb5
akakream <akakream@gmail.com>
2020-12-10 22:09:16 +0300

    new tweet content, some exceptions

--- TUBjobs/bot.py
+++ TUBjobs/bot.py
@@ -12,10 +12,19 @@ class Bot:
 
     def scrape_jobs(self, old_jobs):
         # Get the job posts as set that consists of tuples of job key and link
-        jobs = self.scraper.get_jobs()
+        try:
+            jobs = self.scraper.get_jobs()
+        except:
+            print('TU job server down. Could not get the jobs')
+            jobs = set()
 
         # old_jobs = self.scraper.readFile(os.path.join(pathlib.Path(os.path.abspath(__file__)).parent, 'jobs.txt'))
-        old_jobs = self.scraper.readBlob(old_jobs)
+        try:
+            old_jobs = self.scraper.readBlob(old_jobs)
+        except:
+            print('Could not read the blob')
+            # If reading the old jobs fails, the program will tweet shit load of jobs, this stops that
+            old_jobs = jobs
 
         return jobs, old_jobs
 
@@ -38,12 +47,13 @@ class Bot:
         # Tweet the new jobs
         for job in info_jobs:
             content = self.tweezer.prepare_job_content(job, job_type)
-            self.tweezer.tweet(content)
+            try:
+                status = self.tweezer.tweet(content)
+            except Exception as e:
+                print(e)
         
     def write_jobs(self, jobs, file_name):
         # Write the newly fetched jobs to file
-        try:
-            self.scraper.writeToBlob(jobs, file_name)
-            # self.scraper.writeFile(jobs, os.path.join(pathlib.Path(os.path.abspath(__file__)).parent, file_name))
-        except:
-            print('Could not write to file')
+        self.scraper.writeToBlob(jobs, file_name)
+        # self.scraper.writeFile(jobs, os.path.join(pathlib.Path(os.path.abspath(__file__)).parent, file_name))
+
--- TUBjobs/tweezer.py
+++ TUBjobs/tweezer.py
@@ -11,29 +11,34 @@ class Tweezer():
     
     def prepare_job_content(self, job, job_type):
         content = ''
+
         if job_type == 'new':
             content += '📜 ' + '𝗡𝗘𝗪 𝗝𝗢𝗕!' + ' 📜'  + '\n'
         elif job_type == 'ending':
             content += '😱 ' + '𝗟𝗔𝗦𝗧 𝗖𝗛𝗔𝗡𝗖𝗘 𝗧𝗢 𝗔𝗣𝗣𝗟𝗬!' + ' 😱' + '\n'
-        content += f'𝗙𝗮𝗰𝘂𝗹𝘁𝘆: {job[0][0][1]}\n' 
-        content += f'𝗠𝗼𝗻𝘁𝗵𝗹𝘆 𝗪𝗼𝗿𝗸 𝗛𝗼𝘂𝗿𝘀: {job[0][5][1].split(" ")[0]}\n' 
-        content += f'𝗔𝗽𝗽𝗹𝗶𝗰𝗮𝘁𝗶𝗼𝗻 𝗗𝗲𝗮𝗱𝗹𝗶𝗻𝗲: {job[0][9][1]}\n'
-        content += f'𝗡𝘂𝗺𝗯𝗲𝗿 𝗼𝗳 𝗣𝗼𝘀𝗶𝘁𝗶𝗼𝗻𝘀: {job[0][4][1]}\n'
-        content += f'𝗟𝗶𝗻𝗸: {job[3]}' # link to the job
         
-        # Just in case that the tweet is more than 280 characters
-        if len(content) > 280:
-            content = ''
-            if job_type == 'new':
-                content += '📜 ' + '𝗡𝗘𝗪 𝗝𝗢𝗕!' + ' 📜'  + '\n'
-            elif job_type == 'ending':
-                content += '😱 ' + '𝗟𝗔𝗦𝗧 𝗖𝗛𝗔𝗡𝗖𝗘 𝗧𝗢 𝗔𝗣𝗣𝗟𝗬!' + ' 😱' + '\n'
-            content += f'𝗙𝗮𝗰𝘂𝗹𝘁𝘆: {job[0][0][1]}\n' 
-            content += f'𝗠𝗼𝗻𝘁𝗵𝗹𝘆 𝗪𝗼𝗿𝗸 𝗛𝗼𝘂𝗿𝘀: {job[0][5][1].split(" ")[0]}\n' 
-            content += f'𝗔𝗽𝗽𝗹𝗶𝗰𝗮𝘁𝗶𝗼𝗻 𝗗𝗲𝗮𝗱𝗹𝗶𝗻𝗲: {job[0][9][1]}\n'
-            content += f'𝗟𝗶𝗻𝗸: {job[3]}' # link to the job
-            
+        if len(job[0][0][1]) < 100:
+            content += f'Faculty: {job[0][0][1]}\n' 
+        else:
+            content += f'Faculty: {job[0][0][1][:100]}...\n' 
+        
+        if job[0][3][1] == 'Ja':
+            content += f'Teaching: ✅\n'
+        else:
+            content += f'Teaching: ❌\n'
+        
+        content += f'# Hours/Month: {job[0][5][1].split(" ")[0]}\n' 
+        content += f'# Positions: {job[0][4][1]}\n'
+        content += f'Apply until: {job[0][9][1]}\n'
+
+        # In the worst case the tweet will be 171 characters, excluding the faculty field value
+        # Bold chars and emojis count 2
+        # Link will be cropped only 48 chars are calculated
+        content += f'Link: {job[3]}' # link to the job
+        
         return content
 
     def tweet(self, content):
         status = self.api.update_status(content)
+
+        return status
